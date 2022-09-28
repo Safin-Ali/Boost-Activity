2b189875e20c11672c4056e9f1c119a5d445d5ca
Safin-Ali <safin.ali.7205@gmail.com>
2022-09-28 22:01:59 +0600

    Game Card Box UI Created

---
+++ src/componets/main/game-list/GameList.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+const GameList = () => {
+    return (
+        <div className='row g-4'>
+        </div>
+    );
+};
+
+function CartUI (props) {
+    return(
+    <div className="col-12 col-md-6 col-lg-4">
+        <div>
+            <div className='game-thumb'>
+                <img src="" alt="Game Thumb" />
+            </div>
+            <div className="game-details">
+                <h2>Game Name</h2>
+                <p>Game Description</p>
+                <h4>Age : 18</h4>
+                <h4>Time Requirment</h4>
+            </div>
+            <div>
+                <button>Select Game</button>
+            </div>
+        </div>   
+    </div>
+    )
+}
+
+export default GameList;
